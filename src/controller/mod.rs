f1cfd03401f2598567c8d82bf7eadf2a1df265ca
tobtoby <tobyas2005139@gmail.com>
2025-03-28 18:05:36 +0700

    Implement list function in Notification controller

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -9,7 +9,8 @@ pub fn route_stage() -> AdHoc {
             routes![
                 notification::subscribe,
                 notification::unsubscribe,
-                notification::receive
+                notification::receive,
+                notification::list
             ],
         )
     });
