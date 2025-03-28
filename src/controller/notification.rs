cc9b78dff406300c3558226df9e6825182cccd63
tobtoby <tobyas2005139@gmail.com>
2025-03-28 18:02:41 +0700

    Implement the receive function in Notification controller

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,7 +1,10 @@
 use bambangshop_receiver::Result;
 use rocket::serde::json::Json;
 
-use crate::{model::subscriber::SubscriberRequest, service::notification::NotificationService};
+use crate::{
+    model::{notification::Notification, subscriber::SubscriberRequest},
+    service::notification::NotificationService,
+};
 
 #[get("/unsubscribe/<product_type>")]
 pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
@@ -14,6 +17,14 @@ pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
 #[get("/subscribe/<product_type>")]
 pub fn subscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     return match NotificationService::subscribe(product_type) {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e),
+    };
+}
+
+#[post("/receive", data = "<notification>")]
+pub fn receive(notification: Json<Notification>) -> Result<Json<Notification>> {
+    return match NotificationService::receive_notification(notification.into_inner()) {
         Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e),
     };
